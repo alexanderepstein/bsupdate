51edba99cb2b7ad308e5098b7d2d547304e30827
Alex Epstein <epsteina@wit.edu>
2017-06-19 20:27:10 -0400

    Adding the file initially

---
+++ updateutility.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+currentVersion="" #make sure this is the name you give to the tag for the github release. Also make sure to start each realease tag with v as this is an assumption made by the script but do not give this variable a v.Ex variable set to 1.5.0 github release tag is v1.5.0
+repositoryName=""
+githubUserName=""
+versionReleaseLen=$(echo ${#currentVersion})
+versionReleaseLen=$(expr 10 + $versionReleaseLen )
+
+
+if [[ $(curl -s https://api.github.com/repos/$githubUserName/$repositoryName/tags) == "" ]];then
+   echo "Error: no active internet connection"
+   exit 1
+ fi
+ latestVersion=$(curl -s https://api.github.com/repos/$githubUserName/$repositoryName/tags | grep -Po '"name":.*?[^\\]",'| head -1 | cut -c11-$versionReleaseLen)
+ if [[ "$latestVersion" != "$currentVersion"  && "$latestVersion" != "" ]]; then
+   echo "Version $latestVersion available"
+   echo -n "Do you wish to update $repositoryName [Y/n]: "
+   read answer
+ if [[ "$answer" == "Y" || "$answer" == "y" ]] ;then
+ cd  ~ || { echo 'Update Failed' ; exit 1 ; }
+rm -r $repositoryName  2> /dev/null
+git clone https://github.com/$githubUserName/$repositoryName || { echo "Couldn't download latest version" ; exit 1; }
+cd $repositoryName ||  { echo 'Update Failed' ; exit 1 ;}
+git checkout $latestVersion 2> /dev/null
+#chmod +x install.sh #this might be necessary in your case but wasnt in mine.
+./install.sh
+cd ..
+rm -r $repositoryName #might need to add sudo to this command depending on type of application
+ else
+     exit 1
+ fi
+
+else
+ echo "$repositoryName is already the latest version"
+fi
