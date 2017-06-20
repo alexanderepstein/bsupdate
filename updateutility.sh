9d5a011c88406fe22fa6afd2f1c5d1ec36b03331
Alex Epstein <epsteina@wit.edu>
2017-06-20 05:45:49 -0400

    Formatting with beautysh

--- updateutility.sh
+++ updateutility.sh
@@ -15,26 +15,26 @@ if [[ $currentVersion == "" || $repositoryName == "" || $githubUserName == "" ]]
   exit 1
 
 elif [[ $(curl -s https://api.github.com/repos/$githubUserName/$repositoryName/tags) == "" ]];then
-   echo "Error: no active internet connection" >&2
-   exit 1
+  echo "Error: no active internet connection" >&2
+  exit 1
 else
- latestVersion=$(curl -s https://api.github.com/repos/$githubUserName/$repositoryName/tags | grep -Eo '"name":.*?[^\\]",'| head -1 | cut "-c11-$versionReleaseLen") #to grab the v in your updates change -c11 to -c10
- if [[ "$latestVersion" != "$currentVersion"  && "$latestVersion" != "" ]]; then
+  latestVersion=$(curl -s https://api.github.com/repos/$githubUserName/$repositoryName/tags | grep -Eo '"name":.*?[^\\]",'| head -1 | cut "-c11-$versionReleaseLen") #to grab the v in your updates change -c11 to -c10
+  if [[ "$latestVersion" != "$currentVersion"  && "$latestVersion" != "" ]]; then
     echo "Version $latestVersion available"
     echo -n "Do you wish to update $repositoryName [Y/n]: "
     read -r answer
     if [[ "$answer" == "Y" || "$answer" == "y" ]] ;then
-        cd  ~ || { echo 'Update Failed' ; exit 1 ; }
-        rm -r $repositoryName  2> /dev/null
-        git clone "https://github.com/$githubUserName/$repositoryName" || { echo "Couldn't download latest version" ; exit 1; }
-        cd $repositoryName ||  { echo 'Update Failed' ; exit 1 ;}
-        git checkout "v$latestVersion" 2> /dev/null || git checkout "$latestVersion" 2> /dev/null || echo "Couldn't git checkout to stable release, updating to latest commit."
-        #chmod +x install.sh #this might be necessary in your case but wasnt in mine.
-        ./install.sh
-        cd ..
-        rm -r $repositoryName #might need to add sudo to this command depending on type of application
+      cd  ~ || { echo 'Update Failed' ; exit 1 ; }
+      rm -r $repositoryName  2> /dev/null
+      git clone "https://github.com/$githubUserName/$repositoryName" || { echo "Couldn't download latest version" ; exit 1; }
+      cd $repositoryName ||  { echo 'Update Failed' ; exit 1 ;}
+      git checkout "v$latestVersion" 2> /dev/null || git checkout "$latestVersion" 2> /dev/null || echo "Couldn't git checkout to stable release, updating to latest commit."
+      #chmod +x install.sh #this might be necessary in your case but wasnt in mine.
+      ./install.sh
+      cd ..
+      rm -r $repositoryName #might need to add sudo to this command depending on type of application
     else
-         exit 1
+      exit 1
     fi
 
   else
