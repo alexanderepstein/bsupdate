3658d5c9d1f821ddf9df2b95404f68d7acd526f3
Alex Epstein <epsteina@wit.edu>
2017-06-20 23:34:30 -0400

    No cut use grep a second time, make it easier to change name of install
    Always grab latest version without the v 
    Made it easier to change name of install file by giving it a variable

--- updateutility.sh
+++ updateutility.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # Author: Alexander Epstein https://github.com/alexanderepstein
-# Update utility version 1.0.2
-
-currentVersion="" #make sure this is the name you give to the tag for the github release. Also make sure to start each realease tag with v as this is an assumption made by the script but do not give this variable a v.Ex variable set to 1.5.0 github release tag is v1.5.0
-repositoryName=""
-githubUserName=""
-versionReleaseLen=$(echo ${#currentVersion})
-versionReleaseLen=$((10 + $versionReleaseLen))
-
+# Update utility version 1.1.0
+# To test the tool enter in the defualt values that are in the examples for each variable
+currentVersion="" #This version variable should not have a v but should contain all other characters ex Github release tag is v1.2.4 currentVersion is 1.2.4
+repositoryName="" #Name of repostiory to be updated ex. Sandman-Lite
+githubUserName="" #username that hosts the repostiory ex. alexanderepstein
+nameOfInstallFile="install.sh" # change this if the installer file has a different name be sure to include file extension if there is one
 
 
 if [[ $currentVersion == "" || $repositoryName == "" || $githubUserName == "" ]];then
@@ -18,21 +16,22 @@ elif [[ $(curl -s https://api.github.com/repos/$githubUserName/$repositoryName/t
   echo "Error: no active internet connection" >&2
   exit 1
 else
-  latestVersion=$(curl -s https://api.github.com/repos/$githubUserName/$repositoryName/tags | grep -Eo '"name":.*?[^\\]",'| head -1 | cut "-c11-$versionReleaseLen") #to grab the v in your updates change -c11 to -c10
+  latestVersion=$(curl -s https://api.github.com/repos/$githubUserName/$repositoryName/tags | grep -Eo '"name":.*?[^\\]",'| head -1 | grep -Eo "[0-9].[0-9].*[0-9]*\b" ) #always grabs the tag without the v option
   if [[ "$latestVersion" != "$currentVersion"  && "$latestVersion" != "" ]]; then
     echo "Version $latestVersion available"
     echo -n "Do you wish to update $repositoryName [Y/n]: "
     read -r answer
     if [[ "$answer" == "Y" || "$answer" == "y" ]] ;then
       cd  ~ || { echo 'Update Failed' ; exit 1 ; }
-      rm -r $repositoryName  2> /dev/null
+      echo "Your password is required to continue with the update" #echoing this because of the sudo call
+      sudo rm -r $repositoryName  2> /dev/null #defaulting this command to sudo if you know you will already be root (kali linux) this wont be necessary
       git clone "https://github.com/$githubUserName/$repositoryName" || { echo "Couldn't download latest version" ; exit 1; }
       cd $repositoryName ||  { echo 'Update Failed' ; exit 1 ;}
       git checkout "v$latestVersion" 2> /dev/null || git checkout "$latestVersion" 2> /dev/null || echo "Couldn't git checkout to stable release, updating to latest commit."
       #chmod +x install.sh #this might be necessary in your case but wasnt in mine.
-      ./install.sh
+      "./$nameOfInstallFile"
       cd ..
-      rm -r $repositoryName #might need to add sudo to this command depending on type of application
+      sudo rm -r $repositoryName #defaulting this command to sudo if you know you will already be root (kali linux) this wont be necessary
     else
       exit 1
     fi
